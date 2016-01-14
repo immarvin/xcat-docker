af471aac279d40e32e30d5a4969bfb9b988c79cd
immarvin <yangsbj@cn.ibm.com>
2016-01-14 09:17:59 -0500

    reduce image layers

--- xcat.run/build.sh
+++ xcat.run/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 MYARCH=$(uname -i)
 
 MYARCH=$(echo $MYARCH | tr [A-Z] [a-z])
@@ -7,9 +8,9 @@ MYARCH=$(echo $MYARCH | tr [A-Z] [a-z])
 DOCKFILE=""
 IMAGENAME=""
 
-[ "$MYARCH" = "ppc64le" ] && { DOCKFILE="dockerfile.xcat.ppc64le.run";IMAGENAME="xcat-ppc64le:2.11"; }
+[ "$MYARCH" = "ppc64le" ] && { DOCKFILE="dockerfile.xcat.ppc64le.run";IMAGENAME="xcat/xcat-ubuntu-ppc64le:2.11"; }
 
-[ "$MYARCH" = "x86_64" ] && { DOCKFILE="dockerfile.xcat.x86_64.run";IMAGENAME="xcat-x86_64:2.11"; }
+[ "$MYARCH" = "x86_64" ] && { DOCKFILE="dockerfile.xcat.x86_64.run";IMAGENAME="xcat/xcat-ubuntu-x86_64:2.11"; }
 
 
 [ -n "$DOCKFILE"  ] && [ -n "$IMAGENAME" ] && sudo docker build -t $IMAGENAME -f $DOCKFILE  .
