7712a77764816293ac8500e2709d4cd0f02496b8
immarvin <yangsbj@cn.ibm.com>
2016-01-14 03:17:39 -0500

    stuff to build xcat runtime docker image

---
+++ xcat.run/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MYARCH=$(uname -i)
+
+MYARCH=$(echo $MYARCH | tr [A-Z] [a-z])
+
+DOCKFILE=""
+IMAGENAME=""
+
+[ "$MYARCH" = "ppc64le" ] && { DOCKFILE="dockerfile.xcat.ppc64le.run";IMAGENAME="xcat-ppc64le:2.11"; }
+
+[ "$MYARCH" = "x86_64" ] && { DOCKFILE="dockerfile.xcat.x86_64.run";IMAGENAME="xcat-x86_64:2.11"; }
+
+
+[ -n "$DOCKFILE"  ] && [ -n "$IMAGENAME" ] && sudo docker build -t $IMAGENAME -f $DOCKFILE  .
+
+
+echo "*********************************************"
+echo "Docker Image: $IMAGENAME Created!"
+echo "*********************************************"
