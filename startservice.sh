9396c9557edbcb0ba1e1edd3fa19f4d6d0fa2f36
immarvin <yangsbj@cn.ibm.com>
2016-01-14 21:41:11 -0500

    test

---
+++ startservice.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# This script will be run as the ENTRYPOINT of xCAT docker image
+# to start xcatd and depended services
+
+# Export the xCAT default Env variables
+. /etc/profile.d/xcat.sh
+
+service apache2 start
+
+service ssh start
+
+service isc-dhcp-server start
+
+service rsyslog start
+
+service xcatd start
+
+
+MYIP=$(ip -o -4 addr show dev eth0 |grep eth0|awk -F' ' '{print $4}'|sed -e 's/\/.*//')
+MYHOSTNAME=$(hostname)
+
+([ -n "$MYIP" ] && [ -n "$MYHOSTNAME" ]) && echo "$MYHOSTNAME  $MYIP" >> /etc/hosts
+
+
+
+xcatconfig -d
+
+tabprune networks -a 
+
+makenetworks
+
+bash
